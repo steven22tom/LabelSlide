493b0ae0ec2a1e38bb25e67c2d83f5500a14a38f
steven22tom <btcz720@qq.com>
2021-03-24 09:59:26 +0800

    Update label-box.js

--- static/label-box.js
+++ static/label-box.js
@@ -1,13 +1,31 @@
-function labelBox(config) {
+function LabelBoxRectangle(config) {
     this.id = guid();
     this.classID = null;
     this.shape = this.init(config);
+    this.type = 'rect';
 }
-labelBox.prototype = {
+LabelBoxRectangle.prototype = {
     init: function(config) {
         this.classID = config.tab;
         config.tab = this.id;
         return new fabric.Rect(config); 
+    },
+    set: function(key, value) {
+        this.shape.set(key, value);
+    }
+}
+
+function LabelBoxPolygon(config) {
+    this.id = guid();
+    this.classID = null;
+    this.shape = this.init(config);
+    this.type = 'poly';
+}
+LabelBoxPolygon.prototype = {
+    init: function(config) {
+        this.classID = config.tab;
+        config.tab = this.id;
+        return new fabric.Polygon(config.points, config); 
     },
     set: function(key, value) {
         this.shape.set(key, value);
